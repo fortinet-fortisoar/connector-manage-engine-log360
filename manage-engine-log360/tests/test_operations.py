27e4e7d7b09acc851b5e84210ca68695e1e1bd31
cs-harsh-sharma <sharmah@fortinet.com>
2024-08-09 10:56:02 +0530

    Added copyright comment on test files

--- manage-engine-log360/tests/test_operations.py
+++ manage-engine-log360/tests/test_operations.py
@@ -1,3 +1,10 @@
+"""
+Copyright start
+MIT License
+Copyright (c) 2024 Fortinet Inc
+Copyright end
+"""
+
 # Edit the config_and_params.json file and add the necessary parameter values.
     
 import os
